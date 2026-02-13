9ef85f9a2148ce3800f13b6a25bd3a69eba2f579
ReliefScript <yungtrollbusiness@gmail.com>
2026-02-13 00:44:34 -0500

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,26 @@
+-- Load Library
+local Mogware = loadstring(game:HttpGet("https://raw.githubusercontent.com/ReliefScript/mogware/refs/heads/main/main.lua"))()
+
+-- Window >> Mogware:Window(Name: string)
+local Window = Mogware:Window() -- blank = uses default
+
+-- Tab >> Window:Tab(Name: string)
+local Tab = Window:Tab("Tab")
+
+-- Section >> Tab:Section(Name: string)
+local Section = Tab:Section("Examples")
+
+-- Button >> Section:Button(Name: string, Callback: function)
+Section:Button("Button", function()
+    print("Pressed!")
+end)
+
+-- Dropdown >> Section:Dropdown(Name: string, Options: table, Callback: function)
+Section:Dropdown("Dropdown", {"1", "2", "3"}, function(Option)
+    print(`Option '{Option}' selected!`)
+end)
+
+-- Toggle >> Section:Toggle(Name, Callback)
+Section:Toggle("Toggle", function(Toggled)
+    print(`Toggle set to {Toggled}!`)
+end)
